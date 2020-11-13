45c651e9d388201cd4cb718bc7131c4d35e54212
Ryan Jackson <10ryanjackson10@gmail.com>
2020-11-12 20:35:38 -0800

    rod casting feature

--- src/Components/GameComponents/Rod.js
+++ src/Components/GameComponents/Rod.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react"
+import React, { useRef, useEffect } from "react"
 import { useFrame } from "react-three-fiber"
 import { Html } from 'drei';
 import '../../App.css'
@@ -6,16 +6,36 @@ import Hook from '../../Sprites/hook.png'
 
 function Rod() {
     const rod = useRef();
+    // mouse click triggers casting line
+    var mouseClicked = false;
+    // determines whether or not the line needs to return after its cast
+    var isReturningToInitial = false;
 
+    // may have to change how we do initial positioning because I'm not sure it's consistent across screen sizes
+    useEffect(() => {rod.current.position.set(0,30,0)})
+
+    // DON'T REALLY FEEL TOO GREAT ABOUT ALL THESE IF STATEMENTS IN USEFRAME
     useFrame(({mouse}) => {
-        if (rod.current.position.x + mouse.x < 30 && rod.current.position.x + mouse.x > -30) {
-            rod.current.position.x += mouse.x;
+        if (!mouseClicked && !isReturningToInitial) {
+            if (rod.current.position.x + mouse.x < 30 && rod.current.position.x + mouse.x > -30) {
+                rod.current.position.x += mouse.x;
+            }
         }
-        if (rod.current.position.y + mouse.y < 15 && rod.current.position.y + mouse.y > -10) {
-            rod.current.position.y += mouse.y;
+        else if (mouseClicked && !isReturningToInitial){
+            rod.current.position.y -= .25;
+        }
+        else {
+            if (rod.current.position.y < 30) {
+                rod.current.position.y += .25;
+            }
+            else {isReturningToInitial=false}
         }
     })
-    
+
+    document.addEventListener("mousedown", () => {mouseClicked=true;isReturningToInitial=false})
+    document.addEventListener("mouseup", () => {isReturningToInitial=true;mouseClicked=false})
+
+
     return (
         <group ref={rod}>
         <Html>
@@ -25,4 +45,4 @@ function Rod() {
       )
 }
 
-export default Rod;
+export default Rod; 
