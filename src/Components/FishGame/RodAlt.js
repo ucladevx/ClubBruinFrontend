38890ffd947c9e762dd59f8f230af19ac2c28e41
rohanbattula <rohanbattula@gmail.com>
2021-01-27 02:32:24 -0800

    working fish game for 1 player, rewrote state code

--- src/Components/FishGame/RodAlt.js
+++ src/Components/FishGame/RodAlt.js
@@ -28,86 +28,160 @@ function RodAlt() {
   const [yPos, setyPos] = useState(0);
   const [client, setClient] = useState();
   const [username, _] = useState('user' + Math.random() * 100);
-
+  //const [seshId,setId] = useState();
   const [rodPositions, setRodPositions] = useState({});
-  const [player, setPlayer] = useState({["name"]: {
-    score : 0,
-    x : 0,
-    y : 0,
-  }});
-
   
+/*
+  useEffect(async () => {
+    // console.log(Colyseus);
+    let c = new Colyseus.Client("ws://localhost:9000");
+    setClient(c);
+    c.joinOrCreate("fishgame", {
+    }).then(room => {
+        console.log("setting room");
+        setRoomInstance(room);
 
+        if (room) {
+          console.log(" testing room");
+          room.state.players.onAdd = function (player, sessionId) {
+            
+            console.log("adding player");
+            setPlayer((p)=>({
+              ...p,
+              [sessionId] : {
+                score : player.score,
+                x: player.rod.x,
+                y: player.rod.y,
+              }
+            }));
+            console.log(playerz.x)
+            console.log(playerz.y)
+            player.onChange = function (changes) {
+              changes.forEach(change => {
+                console.log("change in player");
+                console.log(playerz.x)
+                console.log(playerz.y)
+    
+                setPlayer((p)=>({
+                  ...p,
+                  [sessionId] : {
+                    score : player.score,
+                    x: player.rod.x,
+                    y: player.rod.y,
+                  }
+                }));
+            });
+              
+            }
+            player.triggerAll();
+          }
+          
+          room.state.players.onRemove = function (player, sessionId) {
+            setPlayer(null);
+          }
+        }
+    }) 
+  },[]); 
+*/
 
-  useEffect(()=>{
+ 
+  /*
+  useEffect(async () => {
     if (room) {
-      console.log(" testing room");
+      //console.log(" testing room");
       room.state.players.onAdd = function (player, sessionId) {
-        console.log(player.score);
+        console.log("adding player");
         console.log(player.rod.x);
-        console.log(player.rod.y);
-
+        console.log(player.rod.y)
+        console.log(player.score)
         setPlayer((p)=>({
           ...p,
-          [player.username] : {
+          [sessionId] : {
             score : player.score,
             x: player.rod.x,
             y: player.rod.y,
           }
         }));
-      }
 
-      room.state.players.onChange = function (changes) {
-        console.log(player.score);
-        console.log(player.rod.x);
-        console.log(player.rod.y);
+        player.onChange = function (changes) {
+          changes.forEach(change => {
+            console.log("change in player");
+            console.log(player.rod.x)
+            console.log(player.rod.y)
 
-        setPlayer((p)=>({
-          ...p,
-          [player.username] : {
-            score : player.score,
-            x: player.rod.x,
-            y: player.rod.y,
-          }
-        }));
+            setPlayer((p)=>({
+              ...p,
+              [sessionId] : {
+                score : player.score,
+                x: player.rod.x,
+                y: player.rod.y,
+              }
+            }));
+        });
+        }
+
+        player.triggerAll();
       }
+      
       room.state.players.onRemove = function (player, sessionId) {
         setPlayer(null);
       }
     }
   },[room])
+*/
 
   useEffect(() => {
     // Using an IIFE
     (async function anyNameFunction() {
-      let c = new Colyseus.Client("ws://localhost:9000")
+      let c = await new Colyseus.Client("ws://localhost:9000")
       //setClient(c);
-      let room_instance = await c.joinOrCreate("fishgame", {}).then(room => (room))
-      console.log(room_instance)
-      setRoomInstance(room_instance);
+      const room = await c.joinOrCreate("fishgame", {});
+      setRoomInstance(room);
+      room.onStateChange((state) => {
+        setxPos(state.players.get(room.sessionId).rod.x);
+        setyPos(state.players.get(room.sessionId).rod.y);
+
+        
+        
+        console.log(xPos);
+        console.log(yPos)
+        
+      });
       /*
-      if(room){
-        room.onStateChange((state) => {
-          console.log("the room state has been updated:", state);
-          
-        });
+      room.state.players.onAdd = function (player, sessionId) {
+        console.log(player.rod.x);
+        console.log(player.rod.y);
+        
+        player.onChange = function(changes) {
+          changes.forEach(change => {
+            console.log(change.field);
+            console.log(change.value);
+            console.log(change.previousValue);
+        })
+        
+        }
+        player.triggerAll();
+        
       }
-      */
+    */
       
-    })();
-  }, []);
+    
+  })();
+
+  },[]);
+ 
 
   useFrame(({ mouse }) => {
     //console.log(xPos + ": x " + yPos + ": y")
     if (room) {
       if (xPos + mouse.x <= lim && xPos + mouse.x >= -lim && yPos >= 12) {
         //console.log("move left");
-        setxPos(xPos + (mouse.x / 4));
+        //setxPos(xPos + (mouse.x / 4));
         room.send("moveRod", { x: (mouse.x / 4) });
       }
       if (yPos + mouse.y <= 13 && yPos + mouse.y >= -15) {
         //console.log("move right");
-        setyPos(yPos + (mouse.y / 4));
+        //setyPos(yPos + (mouse.y / 4));
         room.send("moveRod", { y: (mouse.y / 4) });
       }
 
@@ -125,10 +199,10 @@ function RodAlt() {
   //console.log(player);
   return (
     <>
-    <Fish x={player["name"].x} y={player["name"].y} pointCount={40}/>
+    <Fish x={xPos} y={yPos} pointCount={40}/>
     
     <mesh
-      position={[player["name"].x,player["name"].y,1]}
+      position={[xPos,yPos,1]}
       geometry={hook.scene.children[2].geometry}
       material={hook.scene.children[2].material}
       scale={[2,2,2]}
@@ -137,7 +211,7 @@ function RodAlt() {
 
 
     <mesh
-     position={[player["name"].x, player["name"].y +18.5, 1]}
+     position={[xPos, yPos +18.5, 1]}
      scale={[.35,1,1]}
      >
   <boxGeometry args={[0.2, 30, 0]}/>
