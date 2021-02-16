db5e6ade59cc3f538b29e1caa01d951fc156be92
Justin Kyle Chang <justinklchang@yahoo.com>
2021-02-15 21:13:43 -0800

    Created SocialLounge page directory

---
+++ src/Components/SocialLounge/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+export default function SocialLounge (props) {
+    
+    return (
+        <div id="content-container">
+            <h1>Join/Create Meeting ID</h1>
+            <input placeholder="MeetingID"></input>
+            <button>Start Meeting</button>
+        </div>
+    )
+}
