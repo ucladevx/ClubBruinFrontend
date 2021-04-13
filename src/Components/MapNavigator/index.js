f83b491df6d10778b6bae44273b4074e47f82467
Justin Kyle Chang <jchangz01@Justins-MacBook-Air.local>
2021-04-12 21:31:01 -0700

    added map navigation functionalities

--- src/Components/MapNavigator/index.js
+++ src/Components/MapNavigator/index.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import './index.css'
+import React, { useState } from 'react';
+import locationsData from './mapLocations.json'
+import './index.css';
 
 export default function MapNavigator () {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+        
+    const entries = locationsData.map ( (location, index) => {
+        {
+            return (
+            <a 
+                href={location.route == currentPath ? null : location.route } 
+                class={`container-entry ${location.route == currentPath ? "active" : null}`}
+                >
+                { /* replace the h1 tag below with
+                 a small image or icon of map entry */ }
+                <h1 class="entry-icon">{index}</h1>
+                <h2 class="entry-name">{location.name}</h2>
+            </a>
+            )
+        }
+    })
+
     return (
         <>
             <div id="container">
                 <div class="container-title-box">
                     <h1 class="container-title">Map Navigator</h1>
                 </div>
-                <a class="container-entry">
-                    <h1 class="entry-icon">1</h1>
-                    <h2 class="entry-name">Inverted Fountain</h2>
-                </a>
-                <a class="container-entry">
-                    <h1 class="entry-icon">1</h1>
-                    <h2 class="entry-name">Inverted Fountain</h2>
-                </a>
-                <a class="container-entry">
-                    <h1 class="entry-icon">1</h1>
-                    <h2 class="entry-name">Inverted Fountain</h2>
-                </a>
-                <a class="container-entry">
-                    <h1 class="entry-icon">1</h1>
-                    <h2 class="entry-name">Inverted Fountain</h2>
-                </a>
+                {entries}
             </div>
             <div class="arrow-down"></div>
         </>
